ff8805375ef08152a147c4ed9f9c9729de9b562c
Quy Dang Ngoc <dangquy95php@gmail.com>
2020-12-05 21:21:33 +0700

    feature/fix_migration_table

---
+++ database/migrations/2020_12_05_141412_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('contacts');
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('address', 100)->nullable();
+            $table->text('note')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
